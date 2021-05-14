603f2c267c6793276f141768976bc0d4cead7ade
Amanda Barrafato <amb1047@nyu.edu>
2021-05-14 08:29:01 -0400

    Feat: makes one sound

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
+// keys
 const keySelector = (e) => {
 	e.target.classList.toggle("selected");
+	if (e.target.classList.contains("selected")) {
+		noteOn(e);
+	}
 };
 
 const keys = Array.from(document.querySelectorAll(".key"));
 
 keys.forEach((key) => key.addEventListener("click", keySelector));
 
+// play button
 const playButton = document.getElementById("play");
 
 const playButtonClicked = (e) => {
@@ -19,3 +24,28 @@ const playButtonClicked = (e) => {
 };
 
 playButton.addEventListener("click", playButtonClicked);
+
+//audio
+let context = null;
+let sound = null;
+
+const getOrCreateContext = () => {
+	if (!context) {
+		context = new AudioContext();
+	}
+
+	return context;
+};
+
+let isStarted = false;
+
+const noteOn = (e) => {
+	const note = document.querySelector(
+		`audio[data-key="${e.target.innerText}"]`
+	);
+
+	if (!note) return;
+
+	note.currentTime = 0;
+	note.play();
+};
